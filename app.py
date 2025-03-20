ab9a7600a8cd22e0970b8f3392cd094a1421a98c
Bluepen-Oficial <victor68346@estudante.ifb.edu.br>
2025-03-19 21:27:18 -0300

    Update app.py

--- app.py
+++ app.py
@@ -82,7 +82,6 @@ def main():
                 lista.clear()
                 lista_sub.clear()
                 lista_pot.clear()
-                variaveis.clear()
                 int_variaveis.clear()
                 reset = True
 
